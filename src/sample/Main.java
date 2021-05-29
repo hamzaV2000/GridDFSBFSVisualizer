f364a61d01590ab7500876f7b11bd3d8a8332e35
hamza <hamzahmdan79@gmail.com>
2021-05-29 20:07:35 +0300

    FirstDate0

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage
+                .setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1000, 900));
         primaryStage.setResizable(false);
         primaryStage.show();
